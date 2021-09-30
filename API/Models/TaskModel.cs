8938f3a86a3ea7a278cebad318816c3581f9da9a
GabrielMarchiorato <gabrielmarchiorato1@outlook.com>
2021-09-30 19:41:44 -0300

    Correção de componentes do BACK

--- API/Models/TaskModel.cs
+++ API/Models/TaskModel.cs
@@ -10,7 +10,9 @@ namespace API.Models
         public int Id { get; set; }
         public string NomeTask { get; set; }
         public string DescricaoTask { get; set; }
-        public DateTime CriadoEm { get; set; }
+        public string Prioridade { get; set; }
+        public string DataTask { get; set; }
+
 
         //ToString
         public override string ToString() => $"Nome: {NomeTask}";
